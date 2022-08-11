2bb7a27730e5d3ac273ceeeff0c9547fb8b6ff0b
joaquintides <joaquin@tid.es>
2022-08-11 19:21:04 +0200

    added clang-arm64 (MacOS)

--- insert_data.sh
+++ insert_data.sh
@@ -15,6 +15,11 @@ python2 insert_data.py -e utf-16 vs-x64/running_erasure.cpp.csv   "Running erasu
 python2 insert_data.py -e utf-16 vs-x64/scattered_lookup.cpp.csv  "Scattered successful lookup"   "vs-x64/Scattered successful looukp.xlsx"   "plot"
 python2 insert_data.py -e utf-16 vs-x64/scattered_lookup.cpp.csv  "Scattered unsuccessful lookup" "vs-x64/Scattered unsuccessful looukp.xlsx" "plot"
 
+python2 insert_data.py clang-arm64/running_insertion.cpp.csv "Running insertion"             "clang-arm64/Running insertion.xlsx"             "plot"
+python2 insert_data.py clang-arm64/running_erasure.cpp.csv   "Running erasure"               "clang-arm64/Running erasure.xlsx"               "plot"
+python2 insert_data.py clang-arm64/scattered_lookup.cpp.csv  "Scattered successful lookup"   "clang-arm64/Scattered successful looukp.xlsx"   "plot"
+python2 insert_data.py clang-arm64/scattered_lookup.cpp.csv  "Scattered unsuccessful lookup" "clang-arm64/Scattered unsuccessful looukp.xlsx" "plot"
+
 python2 insert_data.py gcc-x86/running_insertion.cpp.csv "Running insertion"             "gcc-x86/Running insertion.xlsx"             "plot"
 python2 insert_data.py gcc-x86/running_erasure.cpp.csv   "Running erasure"               "gcc-x86/Running erasure.xlsx"               "plot"
 python2 insert_data.py gcc-x86/scattered_lookup.cpp.csv  "Scattered successful lookup"   "gcc-x86/Scattered successful looukp.xlsx"   "plot"
